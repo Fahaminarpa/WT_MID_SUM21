329da23d6d4962c997f79abec8012f4b77763f0b
Fahamin Arpa <you@example.com>
2021-06-13 15:34:34 +0600

    LAB_3 added

---
+++ LAB_3/Problem-4.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+$name="raju";
+if ( $name =="raju"||$name =="mina"||$name =="mithu")
+{
+    echo '<b> "hello" </b>';
+}
+else {echo "You are not recognized.";}
+?>
+</body>
+</html>
