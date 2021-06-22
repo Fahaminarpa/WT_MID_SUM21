9196b974216b83e2e0dff76566b8d90f18591af7
Fahamin Arpa <you@example.com>
2021-06-22 16:08:42 +0600

    2 ADMIN FORM DONE FOR MID PROJECT

---
+++ MID_PROJECT/admin(dashboard).php
@@ -0,0 +1,36 @@
+<html>
+	<head></head>
+	<body>
+		<form>
+		<fieldset>
+			<table>			
+				<p><b>  ADMIN DASHBOARD </b> </p>
+					
+				<tr>
+				<td><b> INSERTION TYPE </b>
+				
+				
+			<ul>
+						<li><input type="button" value = "Add User"></li>
+						<li><input type="button" value = "Add Station Master"></li>
+						<li> <input type="button" value = "Add Train Master"> </li>
+						
+						
+						<li><input type="button" value = "Add Route Master"></li>
+						<li><input type="button" value = "Delete User"></li>
+						<li> <input type="button" value = "Update User"> </li>
+						
+							<li><input type="button" value = "Update info"></li>
+						<li><input type="button" value = "Update Route"></li>
+						<li> <input type="button" value = "Update Sit"> </li>
+						<li><input type="button" value = "Update Fair"></li>
+						<li> <input type="button" value = "Update Train info"> </li>
+					</ul>
+				</td>
+			</tr>
+			
+				</table>
+		</fieldset>
+		</form>
+	</body>
+</html> 
---
+++ MID_PROJECT/admin(menubar).php
@@ -0,0 +1,25 @@
+<html>
+	<head></head>
+	<body>
+		<form>
+		<fieldset>
+			<table>			
+				<p><b>  ADMIN HOME PAGE </b> </p>
+					
+				<tr>
+				<td><b> MENU BAR </b>
+				
+				
+			<ul>
+						<li><input type="button" value = "Home"></li>
+						<li><input type="button" value = "Dashboard"></li>
+						<li> <input type="button" value = "Log Out"> </li>
+					</ul>
+				</td>
+			</tr>
+			
+				</table>
+		</fieldset>
+		</form>
+	</body>
+</html> 
